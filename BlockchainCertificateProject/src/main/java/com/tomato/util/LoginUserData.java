d660d966d8b315e103fc91a440c6293ea413777d
hahava <hahava5696@gmail.com>
2019-09-12 14:56:25 +0900

    Refactor : LoginUserData
    - Map to List
    - 주석 작성

---
+++ BlockchainCertificateProject/src/main/java/com/tomato/util/LoginUserData.java
@@ -0,0 +1,31 @@
+package com.tomato.util;
+
+import com.tomato.dto.UserDTO;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 데이터 베이스 역할을 담당하는 클래스 <br>
+ * 상속과 객체 생성을 불가능하게 작성
+ *
+ * @author hahava
+ */
+public final class LoginUser {
+
+	private LoginUser() {
+	}
+
+	// 싱글톤 패턴을 사용하여 객체를 관리한다.
+	private final static List<UserDTO> MEMBERS;
+
+	static {
+		MEMBERS = new LinkedList<>();
+		MEMBERS.add(new UserDTO("halin", "1234"));
+		MEMBERS.add(new UserDTO("junghoon", "1234"));
+	}
+
+	public static List<UserDTO> getInstance() {
+		return MEMBERS;
+	}
+}
