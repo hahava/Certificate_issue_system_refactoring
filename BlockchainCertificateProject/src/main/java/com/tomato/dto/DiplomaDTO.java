def93bee83a781dd44240b2c4e7688bdef73a846
hahava <hahava5696@gmail.com>
2019-09-15 12:51:33 +0900

    Refactor : DTO의 역할에 맞춰 함수 수정

--- BlockchainCertificateProject/src/main/java/com/tomato/dto/DiplomaDTO.java
+++ BlockchainCertificateProject/src/main/java/com/tomato/dto/DiplomaDTO.java
@@ -1,124 +1,32 @@
 package com.tomato.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.json.simple.JSONObject;
 
 /**
  * @author hahava
  *
  */
+@Getter
+@Setter
 public class DiplomaDTO {
-	String no;
-	String type;
-	String name;
-	String dateOfBirth;
-	String college;
-	String major;
-	String dateOfMatriculation;
-	String dateOfGraduation;
-	String nameOfDegree;
-	String degreeRegistrationNo;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getNo() {
-		return no;
-	}
-
-	public void setNo(String no) {
-		this.no = no;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getDateOfBirth() {
-		return dateOfBirth;
-	}
-
-	public void setDateOfBirth(String dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-
-	public String getCollege() {
-		return college;
-	}
-
-	public void setCollege(String college) {
-		this.college = college;
-	}
-
-	public String getMajor() {
-		return major;
-	}
-
-	public void setMajor(String major) {
-		this.major = major;
-	}
-
-	public String getDateOfMatriculation() {
-		return dateOfMatriculation;
-	}
-
-	public void setDateOfMatriculation(String dateOfMatriculation) {
-		this.dateOfMatriculation = dateOfMatriculation;
-	}
-
-	public String getDateOfGraduation() {
-		return dateOfGraduation;
-	}
-
-	public void setDateOfGraduation(String dateOfGraduation) {
-		this.dateOfGraduation = dateOfGraduation;
-	}
-
-	public String getNameOfDegree() {
-		return nameOfDegree;
-	}
-
-	public void setNameOfDegree(String nameOfDegree) {
-		this.nameOfDegree = nameOfDegree;
-	}
-
-	public String getDegreeRegistrationNo() {
-		return degreeRegistrationNo;
-	}
-
-	public void setDegreeRegistrationNo(String degreeRegistrationNo) {
-		this.degreeRegistrationNo = degreeRegistrationNo;
-	}
+	private String no;
+	private String type;
+	private String name;
+	private String dateOfBirth;
+	private String college;
+	private String major;
+	private String dateOfMatriculation;
+	private String dateOfGraduation;
+	private String nameOfDegree;
+	private String degreeRegistrationNo;
 
 	@Override
 	public String toString() {
 		return "DiplomaDTO [no=" + no + ", type=" + type + ", name=" + name + ", dateOfBirth=" + dateOfBirth
-				+ ", college=" + college + ", major=" + major + ", dateOfMatriculation=" + dateOfMatriculation
-				+ ", dateOfGraduation=" + dateOfGraduation + ", nameOfDegree=" + nameOfDegree
-				+ ", degreeRegistrationNo=" + degreeRegistrationNo + "]";
+			+ ", college=" + college + ", major=" + major + ", dateOfMatriculation=" + dateOfMatriculation
+			+ ", dateOfGraduation=" + dateOfGraduation + ", nameOfDegree=" + nameOfDegree
+			+ ", degreeRegistrationNo=" + degreeRegistrationNo + "]";
 	}
-
-	public static DiplomaDTO getJsonToDto(JSONObject dataObject) {
-		DiplomaDTO diplomaDTO = new DiplomaDTO();
-		diplomaDTO.setNo(dataObject.get("no").toString());
-		diplomaDTO.setType(dataObject.get("type").toString());
-		diplomaDTO.setName(dataObject.get("name").toString());
-		diplomaDTO.setDateOfBirth(dataObject.get("dateOfBirth").toString());
-		diplomaDTO.setCollege(dataObject.get("college").toString());
-		diplomaDTO.setMajor(dataObject.get("major").toString());
-		diplomaDTO.setDateOfMatriculation(dataObject.get("dateOfMatriculation").toString());
-		diplomaDTO.setDateOfGraduation(dataObject.get("dateOfGraduation").toString());
-		diplomaDTO.setNameOfDegree(dataObject.get("nameOfDegree").toString());
-		diplomaDTO.setDegreeRegistrationNo(dataObject.get("degreeRegistrationNo").toString());
-		return diplomaDTO;
-	}
-
 }
--- BlockchainCertificateProject/src/main/java/com/tomato/dto/EnrollmentDTO.java
+++ BlockchainCertificateProject/src/main/java/com/tomato/dto/EnrollmentDTO.java
@@ -16,18 +16,4 @@ public class EnrollmentDTO {
 	private String college;
 	private String major;
 	private String grade;
-
-	public static EnrollmentDTO getJsonToDto(JSONObject dataObject) {
-		EnrollmentDTO enrollmentDTO = new EnrollmentDTO();
-		enrollmentDTO.setNo(dataObject.get("no").toString());
-		enrollmentDTO.setType(dataObject.get("type").toString());
-		enrollmentDTO.setName(dataObject.get("name").toString());
-		enrollmentDTO.setDateOfBirth(dataObject.get("dateOfBirth").toString());
-		enrollmentDTO.setCollege(dataObject.get("college").toString());
-		enrollmentDTO.setMajor(dataObject.get("major").toString());
-		enrollmentDTO.setGrade(dataObject.get("grade").toString());
-		return enrollmentDTO;
-
-	}
-
 }
