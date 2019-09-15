38502e771f916ef4c3fb999cd15ae0f8645df1cc
hahava <hahava5696@gmail.com>
2019-09-15 13:46:23 +0900

    Refactor : @lombok 적용

--- BlockchainCertificateProject/src/main/java/com/tomato/dto/EmailDTO.java
+++ BlockchainCertificateProject/src/main/java/com/tomato/dto/EmailDTO.java
@@ -1,35 +1,13 @@
 package com.tomato.dto;
 
-public class EmailDTO {
-	 	private String subject;
-	    private String content;
-	    private String regdate;
-	    private String reciver;
-	     
-	    public String getReciver() {
-	        return reciver;
-	    }
-	    public void setReciver(String reciver) {
-	        this.reciver = reciver;
-	    }
+import lombok.Getter;
+import lombok.Setter;
 
-	    public String getSubject() {
-	        return subject;
-	    }
-	    public void setSubject(String subject) {
-	        this.subject = subject;
-	    }
-	    public String getContent() {
-	        return content;
-	    }
-	    public void setContent(String content) {
-	        this.content = content;
-	    }
-	    public String getRegdate() {
-	        return regdate;
-	    }
-	    public void setRegdate(String regdate) {
-	        this.regdate = regdate;
-	    }
-	     
+@Getter
+@Setter
+public class EmailDTO {
+	private String subject;
+	private String content;
+	private String regdate;
+	private String reciver;
 }
